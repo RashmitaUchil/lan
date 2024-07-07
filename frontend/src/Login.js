85e18d2ead0f5cb55af53e15f540484051c1c282
unknown <rashmitauchil20212@gmail.com>
2024-07-07 15:03:04 +0530

    final

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -35,7 +35,7 @@ const Login = () => {
         }
         console.log(res);
     })
-    .catch(err=>toast.error(err.message) );
+    .catch(err=>toast.error("email or password is incorrect") );
     // Redirect to dashboard or desired page after login
     
   };
